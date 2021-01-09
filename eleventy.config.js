87521bc3ef0cfa5aee900440c9fe30c2ee46545c
Sascha Englert <sascha@englerts.de>
2021-01-09 12:29:42 +0100

    Updated eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/static')
 
   return {
-    dir: { input: 'src', output: '_site' }
+    dir: {
+      input: 'src',
+      output: '_site',
+      layouts: "_includes/layouts"
+    },
+    templateFormats: ["njk"]
   };
 };
